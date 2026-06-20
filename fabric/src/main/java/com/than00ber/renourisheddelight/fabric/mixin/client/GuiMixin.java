573cc932089d1af4bc8432e160d1c24bdec84cdd
Noah Greff <greffnoah@gmail.com>
2026-06-20 17:20:09 +0200

    fixed air bubble y offset on fabric

--- fabric/src/main/java/com/than00ber/renourisheddelight/fabric/mixin/client/GuiMixin.java
+++ fabric/src/main/java/com/than00ber/renourisheddelight/fabric/mixin/client/GuiMixin.java
@@ -3,26 +3,22 @@ package com.than00ber.renourisheddelight.fabric.mixin.client;
 import com.than00ber.renourisheddelight.food.DietHolder;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Gui.class)
 public abstract class GuiMixin {
 
-    @Shadow protected abstract int getVehicleMaxHearts(LivingEntity entity);
-
-    @Redirect(method = "renderPlayerHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;getVehicleMaxHearts(Lnet/minecraft/world/entity/LivingEntity;)I"))
-    private int renderPlayerHealth(Gui gui, LivingEntity entity) {
-        int hearts = getVehicleMaxHearts(entity);
-        return hearts > 0 ? hearts : -1;
+    @Inject(method = "renderFood", at = @At("HEAD"), cancellable = true)
+    private void renderFood(CallbackInfo callback) {
+        callback.cancel();
     }
 
-    @ModifyVariable(method = "renderPlayerHealth", at = @At("STORE"), name = "t", ordinal = 10)
-    private int renderPlayerHealth(int t) {
-        return Minecraft.getInstance().player instanceof DietHolder holder && !holder.getDiet().getSlots().isEmpty() ? t - 5 : t;
+    @ModifyArg(method = "renderPlayerHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lnet/minecraft/resources/ResourceLocation;IIII)V"), index = 2)
+    private int renderPlayerHealth(int y) {
+        return Minecraft.getInstance().player instanceof DietHolder holder && !holder.getDiet().getSlots().isEmpty() ? y : y + 10;
     }
 }
--- neoforge/src/main/java/com/than00ber/renourisheddelight/neoforge/event/RegistryEvent.java
+++ neoforge/src/main/java/com/than00ber/renourisheddelight/neoforge/event/RegistryEvent.java
@@ -6,7 +6,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterClientReloadListenersEvent;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public final class RegistryEvent {
 
     @SubscribeEvent
