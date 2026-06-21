003010ccca73c6091e1e88173ff91153996ae4c4
Noah Greff <greffnoah@gmail.com>
2026-06-21 14:02:50 +0200

    added comments

--- common/src/main/java/com/than00ber/renourisheddelight/Configuration.java
+++ common/src/main/java/com/than00ber/renourisheddelight/Configuration.java
@@ -9,11 +9,6 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 @Config(name = RenourishedDelightMod.MOD_ID)
 public class Configuration implements ConfigData {
 
-    @Comment("Horizontal pixel offset for the food display UI (default: 0)")
-    public int foodBarOffsetX = 0;
-    @Comment("Vertical pixel offset for the food display UI (default: 0)")
-    public int foodBarOffsetY = 0;
-
     public static Configuration getInstance() {
         return AutoConfig.getConfigHolder(Configuration.class).getConfig();
     }
@@ -21,4 +16,9 @@ public class Configuration implements ConfigData {
     public static void init() {
         AutoConfig.register(Configuration.class, JanksonConfigSerializer::new);
     }
+
+    @Comment("Horizontal pixel offset for the food display UI (default: 0)")
+    public int foodBarOffsetX = 0;
+    @Comment("Vertical pixel offset for the food display UI (default: 0)")
+    public int foodBarOffsetY = 0;
 }
