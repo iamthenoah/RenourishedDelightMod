d419886a45a653be5016c2ddb5ac3b0dc1c01004
Noah Greff <greffnoah@gmail.com>
2026-06-21 14:02:33 +0200

    added comments

--- common/src/main/java/com/than00ber/renourisheddelight/Configuration.java
+++ common/src/main/java/com/than00ber/renourisheddelight/Configuration.java
@@ -3,19 +3,22 @@ package com.than00ber.renourisheddelight;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = RenourishedDelightMod.MOD_ID)
 public class Configuration implements ConfigData {
 
-    public static void init() {
-        AutoConfig.register(Configuration.class, GsonConfigSerializer::new);
-    }
+    @Comment("Horizontal pixel offset for the food display UI (default: 0)")
+    public int foodBarOffsetX = 0;
+    @Comment("Vertical pixel offset for the food display UI (default: 0)")
+    public int foodBarOffsetY = 0;
 
     public static Configuration getInstance() {
         return AutoConfig.getConfigHolder(Configuration.class).getConfig();
     }
 
-    public int foodBarOffsetX = 0;
-    public int foodBarOffsetY = 0;
+    public static void init() {
+        AutoConfig.register(Configuration.class, JanksonConfigSerializer::new);
+    }
 }
