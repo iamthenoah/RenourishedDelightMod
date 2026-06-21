7cb898b801259899a55d17785f91ff8a4541f4f3
Noah Greff <greffnoah@gmail.com>
2026-06-21 13:03:05 +0200

    fixed resources not loading

--- neoforge/src/main/java/com/than00ber/renourisheddelight/neoforge/event/RegistryEvent.java
+++ neoforge/src/main/java/com/than00ber/renourisheddelight/neoforge/event/RegistryEvent.java
@@ -4,6 +4,7 @@ import com.than00ber.renourisheddelight.client.atlas.TextureAtlasResourceLoader;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterClientReloadListenersEvent;
+import net.neoforged.neoforge.event.AddReloadListenerEvent;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public final class RegistryEvent {
@@ -11,5 +12,10 @@ public final class RegistryEvent {
     @SubscribeEvent
     public static void onRegisterReloadListeners(RegisterClientReloadListenersEvent event) {
         event.registerReloadListener(TextureAtlasResourceLoader.getInstance());
+    }
+
+    @SubscribeEvent
+    public static void onAddReloadListenerEvent(AddReloadListenerEvent event) {
+        event.addListener(TextureAtlasResourceLoader.getInstance());
     }
 }
