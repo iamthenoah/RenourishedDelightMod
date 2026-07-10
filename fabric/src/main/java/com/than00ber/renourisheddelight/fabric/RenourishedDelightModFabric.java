f3e785d40c8c998a0d82bd40bd37be7a86fc5e97
Noah Greff <greffnoah@gmail.com>
2026-07-10 17:17:28 +0200

    updated fabric init methods

--- fabric/src/main/java/com/than00ber/renourisheddelight/fabric/RenourishedDelightModFabric.java
+++ fabric/src/main/java/com/than00ber/renourisheddelight/fabric/RenourishedDelightModFabric.java
@@ -1,19 +1,16 @@
 package com.than00ber.renourisheddelight.fabric;
 
 import com.than00ber.renourisheddelight.RenourishedDelightMod;
-import com.than00ber.renourisheddelight.client.atlas.TextureAtlasResourceLoader;
 import com.than00ber.renourisheddelight.effect.NourishmentMobEffect;
 import com.than00ber.renourisheddelight.food.Diet;
 import com.than00ber.renourisheddelight.registry.EffectRegistry;
 import com.than00ber.renourisheddelight.registry.PotionRegistry;
-import dev.architectury.registry.ReloadListenerRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.PackType;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.Potion;
@@ -24,7 +21,6 @@ public final class RenourishedDelightModFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         EntityDataSerializers.registerSerializer(Diet.DATA_SERIALIZER);
-        ReloadListenerRegistry.register(PackType.CLIENT_RESOURCES, TextureAtlasResourceLoader.getInstance());
         EffectRegistry.NOURISHMENT = BuiltInRegistries.MOB_EFFECT.wrapAsHolder(Registry.register(
                 BuiltInRegistries.MOB_EFFECT,
                 ResourceLocation.fromNamespaceAndPath(RenourishedDelightMod.MOD_ID, "nourishment"),
