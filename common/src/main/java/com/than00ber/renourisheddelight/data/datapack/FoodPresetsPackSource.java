83998c5aab3f3e9e2a3c4bb4f2c2edfc480d54fa
Noah Greff <greffnoah@gmail.com>
2026-09-07 08:30:07 +0200

    fixed bug

--- common/src/main/java/com/than00ber/renourisheddelight/data/datapack/FoodPresetsPackSource.java
+++ common/src/main/java/com/than00ber/renourisheddelight/data/datapack/FoodPresetsPackSource.java
@@ -22,7 +22,7 @@ public final class FoodPresetsPackSource implements RepositorySource {
 
         if (root != null) {
             String location = "mod/" + FoodConfigReloadListener.PRESETS_KEY;
-            Component name = Component.translatable("pack." + RenourishedDelightMod.MOD_ID + ".presets");
+            Component name = Component.translatable("pack." + RenourishedDelightMod.MOD_ID + ".foodpresets");
             PackLocationInfo info = new PackLocationInfo(location, name, PackSource.BUILT_IN, Optional.empty());
             Pack.ResourcesSupplier supplier = getResourcesSupplier(info, root);
             PackSelectionConfig packet = new PackSelectionConfig(false, Pack.Position.TOP, false);
